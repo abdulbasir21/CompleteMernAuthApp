cb64436fe7499fd012dbe64cb640d61326ea11f9
Abdul Basir <abdulbasir4211@gmail.com>
2025-11-11 12:38:23 +0500

    Refactor CORS configuration and allowed origins

--- server/server.js
+++ server/server.js
@@ -6,25 +6,36 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
-
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 mongodb();
 
-const allowedOrigins = 'https://complete-mern-auth-app.vercel.app'
-app.use(express.json());
+const allowedOrigins = [
+  'https://complete-mern-auth-app.vercel.app',
+  'http://localhost:5173'
+];
 
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: allowedOrigins}))
+
+app.use(cors({
+  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.get("/", (req, res) => {
     res.send("API is running....");
 });
 
-//Api endpoints
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
-app.listen(port, () => console.log(`Server started on PORT:${port}`)
-);
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
